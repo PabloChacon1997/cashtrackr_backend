d13eeb11118be48e9a42a02b5515d13b1ad6d19b
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-03-09 21:51:52 -0500

    config BD and dotenv

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express' 
+import colors from 'colors'
+import morgan from 'morgan'
+import { db } from './config/db'
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync()
+    console.log(colors.blue.bold('Conexion exitosa a la BD'))
+  } catch (error) {
+    console.log(colors.red.bold('Fallo la conexion a la BD'));
+  }
+}
+
+connectDB()
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+
+
+export default app
